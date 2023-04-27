42beeb89182cc9addc09599405b785a38f0e2e2e
Vasilich563 <yackub563@mail.ru>
2023-04-27 21:38:19 +0300

    unsignificant changes in py-scripts (comape_task2.py, converte_to_decimal_task1.py)

--- lab_8/main.cpp
+++ lab_8/main.cpp
@@ -18,15 +18,26 @@ void constantZeroLogicalFunctionTest(std::fstream& logout)
 {
    logout << "constantZeroLogicalFunctionTest\n";
    DiagonalAM test_DAM;
+   logout << "Matrix:\n";
    logout << test_DAM << std::endl;
+   logout << "############################################\n";
+   logout << "Words:\n";
+   for(int i = 0; i < test_DAM.size(); i++)
+      logout << i << ")\t" << test_DAM.getWord(i) << std::endl;
    logout << "############################################\n";
    int first_arg_index = rand() %16;
    int second_arg_index = rand() %16;
    int result_index = rand() %16;
+   logout << "first argument:\t" << test_DAM.getWord(first_arg_index) << "second argiment:\t" << test_DAM.getWord(second_arg_index) << std::endl;
    test_DAM.constantZeroLF(first_arg_index, second_arg_index, result_index);
+   logout << "result word:\t" << test_DAM.getWord(result_index) << std::endl;
    logout << "After f0(" << first_arg_index << "; " << second_arg_index << "). Result emplaced at: " <<  result_index  << " index\n";
    logout << test_DAM << std::endl;
    logout << "\n############################################\n\n";
+   logout << "Words:\n";
+   for(int i = 0; i < test_DAM.size(); i++)
+      logout << i << ")\t" << test_DAM.getWord(i) << std::endl;
+   logout << "############################################\n";
 }
 
 
@@ -34,15 +45,26 @@ void constantOneLogicalFunctionTest(std::fstream& logout)
 {
    logout << "constantOneLogicalFunctionTest\n";
    DiagonalAM test_DAM;
+   logout << "Matrix:\n";
    logout << test_DAM << std::endl;
+   logout << "############################################\n";
+   logout << "Words:\n";
+   for(int i = 0; i < test_DAM.size(); i++)
+      logout << i << ")\t" << test_DAM.getWord(i) << std::endl;
    logout << "############################################\n";
    int first_arg_index = rand() % 16;
    int second_arg_index = rand() % 16;
    int result_index = rand() % 16;
+   logout << "first argument:\t" << test_DAM.getWord(first_arg_index) << "second argiment:\t" << test_DAM.getWord(second_arg_index) << std::endl;
    test_DAM.constantOneLF(first_arg_index, second_arg_index, result_index);
+   logout << "result word:\t" << test_DAM.getWord(result_index) << std::endl;
    logout << "After f15(" << first_arg_index << "; " << second_arg_index << "). Result emplaced at: " <<  result_index  << " index\n";
    logout << test_DAM << std::endl;
    logout << "\n############################################\n\n";
+   logout << "Words:\n";
+   for(int i = 0; i < test_DAM.size(); i++)
+      logout << i << ")\t" << test_DAM.getWord(i) << std::endl;
+   logout << "############################################\n";
 }
 
 
@@ -50,15 +72,26 @@ void repeatSecondFunctionTest(std::fstream& logout)
 {
    logout << "repeatSecondFunctionTest\n";
    DiagonalAM test_DAM;
+   logout << "Matrix:\n";
    logout << test_DAM << std::endl;
+   logout << "############################################\n";
+   logout << "Words:\n";
+   for(int i = 0; i < test_DAM.size(); i++)
+      logout << i << ")\t" << test_DAM.getWord(i) << std::endl;
    logout << "############################################\n";
    int first_arg_index = rand() % 16;
    int second_arg_index = rand() % 16;
    int result_index = rand() % 16;
+   logout << "first argument:\t" << test_DAM.getWord(first_arg_index) << "second argiment:\t" << test_DAM.getWord(second_arg_index) << std::endl;
    test_DAM.repeatSecondArgumentLF(first_arg_index, second_arg_index, result_index);
+   logout << "result word:\t" << test_DAM.getWord(result_index) << std::endl;
    logout << "After f5(" << first_arg_index << "; " << second_arg_index << "). Result emplaced at: " <<  result_index  << " index\n";
    logout << test_DAM << std::endl;
    logout << "\n############################################\n\n";
+   logout << "Words:\n";
+   for(int i = 0; i < test_DAM.size(); i++)
+      logout << i << ")\t" << test_DAM.getWord(i) << std::endl;
+   logout << "############################################\n";
 }
 
 
@@ -66,15 +99,26 @@ void negateSecondLogicalFunctionTest(std::fstream& logout)
 {
    logout << "negateSecondLogicalFunctionTest\n";
    DiagonalAM test_DAM;
+   logout << "Matrix:\n";
    logout << test_DAM << std::endl;
+   logout << "############################################\n";
+   logout << "Words:\n";
+   for(int i = 0; i < test_DAM.size(); i++)
+      logout << i << ")\t" << test_DAM.getWord(i) << std::endl;
    logout << "############################################\n";
    int first_arg_index = rand() % 16;
    int second_arg_index = rand() % 16;
    int result_index = rand() % 16;
+   logout << "first argument:\t" << test_DAM.getWord(first_arg_index) << "second argiment:\t" << test_DAM.getWord(second_arg_index) << std::endl;
    test_DAM.negateSecondArgumentLF(first_arg_index, second_arg_index, result_index);
+   logout << "result word:\t" << test_DAM.getWord(result_index) << std::endl;
    logout << "After f10(" << first_arg_index << "; " << second_arg_index << "). Result emplaced at: " <<  result_index  << " index\n";
    logout << test_DAM << std::endl;
    logout << "\n############################################\n\n";
+   logout << "Words:\n";
+   for(int i = 0; i < test_DAM.size(); i++)
+      logout << i << ")\t" << test_DAM.getWord(i) << std::endl;
+   logout << "############################################\n";
 }
 
 
@@ -106,16 +150,18 @@ void searchTest()
       for(int i = 0; i < test_DAM.size(); i++)
          word_to_search[i] = rand() % 2;
 
+      logout << "Matrix:\n";
       logout << test_DAM << std::endl;
 
       auto accordance_search_result = test_DAM.accordanceSearch(word_to_search);
       logout<< "############################################\n";
 
-      logout << "wts: " << word_to_search << std::endl;
-      logout << "asr: " << accordance_search_result << std::endl;
+      logout << "word to search:\t" << word_to_search << std::endl;
+      logout << "accordance search result:\t" << accordance_search_result << std::endl;
       logout<< "############################################\n";
+      logout << "Words:\n";
       for(int i = 0; i < test_DAM.size(); i++)
-         logout << test_DAM.getWord(i) << std::endl;
+         logout << i << ")\t" << test_DAM.getWord(i) << std::endl;
       logout << "\n############################################\n\n";
    }
    logout.close();
@@ -133,20 +179,23 @@ void addOperationTest()
       for(int i = 0; i < test_DAM.size(); i++)
          word_to_search[i] = rand() % 2;
 
+      logout << "Matrix:\n";
       logout << test_DAM << std::endl;
       logout << "############################################\n";
+      logout << "Words:\n";
       for(int i = 0; i < test_DAM.size(); i++)
-         logout << test_DAM.getWord(i) << std::endl;
+         logout << i << ")\t" << test_DAM.getWord(i) << std::endl;
       logout << "############################################\n";
-      logout << word_to_search << std::endl;
+      logout << "word to search:\t" << word_to_search << std::endl;
       logout << "############################################\n";
 
       int res_amount = test_DAM.makeAddition(word_to_search);
-      logout << "Add operation made for: " << res_amount << std::endl;
+      logout << "Add operation made for: " << res_amount << std::endl << "Matrix after operations:\n";
       logout << test_DAM << std::endl;
       logout << "############################################\n";
+      logout << "Words:\n";
       for(int i = 0; i < test_DAM.size(); i++)
-         logout << test_DAM.getWord(i) << std::endl;
+         logout << i << ")\t" << test_DAM.getWord(i) << std::endl;
    }
    logout.close();
 }
